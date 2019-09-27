f249c917609bd777c30ec6a733f45723651a4c9b
Dante Spencer <spencerdantel@gmail.com>
2019-09-27 13:02:32 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,7 +1,6 @@
-def TABLE
-;CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT, sub_genre_id, characters_id);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year TEXT, series_id);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
-CREATE TABLE"
+CREATE TABLE
